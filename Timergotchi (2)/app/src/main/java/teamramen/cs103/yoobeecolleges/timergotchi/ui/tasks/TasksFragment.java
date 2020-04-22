ecb8684d42b3504e9dbba5ab612c2ba8bdee565c
Manny <teamramenyoobee@gmail.com>
2020-04-22 13:52:28 +1200

    SPLASH SCREEN

---
+++ Timergotchi (2)/app/src/main/java/teamramen/cs103/yoobeecolleges/timergotchi/ui/tasks/TasksFragment.java
@@ -0,0 +1,28 @@
+package teamramen.cs103.yoobeecolleges.timergotchi.ui.tasks;
+
+        import android.os.Bundle;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.TextView;
+
+        import androidx.annotation.Nullable;
+        import androidx.annotation.NonNull;
+        import androidx.fragment.app.Fragment;
+        import androidx.lifecycle.Observer;
+        import androidx.lifecycle.ViewModelProviders;
+
+        import teamramen.cs103.yoobeecolleges.timergotchi.R;
+
+public class TasksFragment extends Fragment {
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_tasks, container, false);
+
+        return root;
+    }
+}
